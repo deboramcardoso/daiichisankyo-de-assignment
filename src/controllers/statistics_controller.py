752efcc7f5f1caf676c8779970081e378d806341
Débora Cardoso <deboram.cardoso@gmail.com>
2024-09-14 16:15:44 +0200

    Enable optional filtering by date range and channel in customer interactions API

--- src/controllers/statistics_controller.py
+++ src/controllers/statistics_controller.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, jsonify, request
 from ..factory import get_statistics_repository
 from collections import defaultdict
 
 
 stats_blueprint = Blueprint("stats_blueprint", __name__)
+
 
 @stats_blueprint.route("/api/v1/stats/<table_name>", methods=["GET"])
 def table_count(table_name):
@@ -13,22 +14,28 @@ def table_count(table_name):
         return jsonify({"message": "Number of Rows", "rows": row}), 200
     return jsonify({"message": "Table name not found"}), 404
 
+
 @stats_blueprint.route("/api/v1/stats/interactions/<customer_id>")
 def customer_interactions_per_channel_count(customer_id):
+
+    # Get optional date filters from the query parameters
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+    channel = request.args.get("channel")
+
     stats_repo = get_statistics_repository(current_app.config)
-    query_result = stats_repo.get_total_interactions_per_customer_and_channel(customer_id)
+    query_result = stats_repo.get_total_interactions_per_customer_and_channel(
+        customer_id, start_date, end_date, channel
+    )
 
     result = {"data": {}}
     interactions = {}
-    
+
     for record in query_result:
         event = record.get("event")
         total_interactions = record.get("total_interactions")
         interactions[event] = total_interactions
 
-    result["data"] = {
-        "customer_id": customer_id,
-        "interactions": interactions
-    }
+    result["data"] = {"customer_id": customer_id, "interactions": interactions}
 
     return jsonify(result), 200
--- src/repository/statistics/postgres.py
+++ src/repository/statistics/postgres.py
@@ -5,28 +5,7 @@ from ...config import Config
 TABLES = ["customers", "products", "interactions"]
 
 TABLE_COUNT_SQL = """SELECT COUNT(*) FROM {table_name}; """
-CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL= """
-WITH all_events AS
-(
-SELECT
-    DISTINCT event
-    FROM interactions
-)
 
-SELECT
-    c.customer_id,
-    e.event,
-    SUM(CASE WHEN i.event IS NOT NULL THEN 1 ELSE 0 END) AS total_interactions
-FROM customers AS c
-CROSS JOIN all_events AS e
-LEFT JOIN interactions AS i
-ON e.event = i.event
-AND c.customer_id = i.customer_id
-WHERE c.customer_id = {customer_id}
-GROUP BY
-    c.customer_id,
-    e.event
-"""
 
 class PostgresStatisticsRepository(statisticsRepository):
     def __init__(self, config: Config):
@@ -40,18 +19,60 @@ class PostgresStatisticsRepository(statisticsRepository):
             Tuple with the number of content entries.
         """
 
-        result = self.runner.execute_query(TABLE_COUNT_SQL.format(table_name = table_name), return_dict=False)
+        result = self.runner.execute_query(
+            TABLE_COUNT_SQL.format(table_name=table_name), return_dict=False
+        )
         return result[0][0]
-    
+
     def check_table_name(self, table_name) -> bool:
         return table_name in TABLES
-    
-    def get_total_interactions_per_customer_and_channel(self, customer_id):
+
+    def get_total_interactions_per_customer_and_channel(self, customer_id, start_date=None, end_date=None, channel=None):
         """
         Compute the number of interactions per customer per channel.
 
         Returns:
 
         """
-        result = self.runner.execute_query(CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL.format(customer_id = customer_id))
+        CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL = """
+        WITH all_events AS
+        (
+        SELECT
+            DISTINCT event
+            FROM interactions
+        )
+
+        SELECT
+            c.customer_id,
+            e.event,
+            SUM(CASE WHEN i.event IS NOT NULL THEN 1 ELSE 0 END) AS total_interactions
+        FROM customers AS c
+        CROSS JOIN all_events AS e
+        LEFT JOIN interactions AS i
+        ON e.event = i.event
+        AND c.customer_id = i.customer_id
+        """
+
+        #Apply date filters if provided
+        if start_date and end_date:
+            CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL += f" AND i.date_start >= '{start_date}' AND i.date_start <= '{end_date}'"
+        elif start_date:
+            CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL += f" AND i.date_start >= '{start_date}'"
+        elif end_date:
+            CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL += f" AND i.date_start <= '{end_date}'"
+
+        #Apply channel filter if provided
+        if channel:
+            CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL += f" AND i.event = '{channel}'"
+
+        CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL += """
+        WHERE c.customer_id = {customer_id}
+        GROUP BY
+            c.customer_id,
+            e.event
+        """
+
+        result = self.runner.execute_query(
+            CUSTOMER_INTERACTIONS_PER_CHANNEL_SQL.format(customer_id=customer_id)
+        )
         return result
